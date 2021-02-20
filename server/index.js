8230a9f38f5913d2e192e4d4d1fd46b533238713
alex <alex_kobilianskiy@bigmir.net>
2021-02-20 14:35:21 +0200

    install express, mongoose, config; create mongoDb connection; create User model;

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
-console.log('server started!');
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const app = express();
+const PORT = config.get('serverPort');
+
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get("dbUrl"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+
+    app.listen(PORT, ()=>{
+      console.log('server started on port ', PORT)})
+  } catch (e) {
+    console.log(e.message)
+  }
+}
+
+start();
