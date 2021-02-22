c3c5eb07d4d496a5d7c14f329a02d9be0e598bcd
alex <alex_kobilianskiy@bigmir.net>
2021-02-22 11:33:27 +0200

    create login form

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,34 @@
+import React, {useEffect} from 'react';
 import Navbar from './navbar/Navbar';
 import './app.scss';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import Registration from './registration/Registration';
+import Registration from './authorization/Registration';
+import Login from './authorization/Login';
+import {useDispatch, useSelector} from 'react-redux';
+import {auth} from '../actions/user';
 
 function App() {
+  const isAuth = useSelector(state => state.user.isAuth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    try {
+      dispatch(auth())
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  }, [])
+
   return (
     <BrowserRouter>
       <div className="app">
         <Navbar />
         <div className="wrap">
-          <Switch>
+          {!isAuth && <Switch>
             <Route path='/registration' component={Registration}/>
-          </Switch>
+            <Route path='/login' component={Login}/>
+          </Switch>}
+
         </div>
 
       </div>
