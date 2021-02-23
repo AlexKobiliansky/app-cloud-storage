4f2d6e15233b2ad3dd0f00e7864826c8651de904
alex <alex_kobilianskiy@bigmir.net>
2021-02-23 11:58:17 +0200

    loading files to server (front-end)

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {createDir, getFiles} from '../../actions/file';
+import {createDir, getFiles, uploadFile} from '../../actions/file';
 import FileList from './FileList/FileList';
 import './disk.scss';
 import Popup from './Popup';
@@ -25,11 +25,20 @@ function Disk() {
     dispatch(setCurrentDir(backDirId));
   }
 
+  function fileUploadHandler(event) {
+    const files = [...event.target.files];
+    files.forEach(file => dispatch(uploadFile(file, currentDir)));
+  }
+
   return (
     <div className="disk">
       <div className="disk_btns">
         <button className="disk__back" onClick={() => backClickHandler()}>Назад</button>
         <button className="disk__create" onClick={() => showPopupHandler()}>Создать папку</button>
+        <div className="disk__upload">
+          <label htmlFor="disk__upload-input" className="disk__upload-label">Загрузить файл</label>
+          <input multiple={true} onChange={(event) => fileUploadHandler(event)} type="file" className="disk__upload-input" id="disk__upload-input"/>
+        </div>
       </div>
       <FileList />
       <Popup />
